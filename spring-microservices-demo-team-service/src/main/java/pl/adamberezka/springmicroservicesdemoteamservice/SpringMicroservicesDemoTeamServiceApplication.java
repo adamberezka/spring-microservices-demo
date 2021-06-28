0e2fbe8debddf51b14470f49f7b1a8f5741f0d53
Adam Berezka <adam.berezka99@gmail.com>
2021-06-28 22:01:32 +0200

    Add eureka server and client annotations and team service init

--- spring-microservices-demo-team-service/src/main/java/pl/adamberezka/springmicroservicesdemoteamservice/SpringMicroservicesDemoTeamServiceApplication.java
+++ spring-microservices-demo-team-service/src/main/java/pl/adamberezka/springmicroservicesdemoteamservice/SpringMicroservicesDemoTeamServiceApplication.java
@@ -1,4 +1,4 @@
-package pl.adamberezka.springmicroservicesdemouserservice;
+package pl.adamberezka.springmicroservicesdemoteamservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class SpringMicroservicesDemoUserServiceApplication {
+public class SpringMicroservicesDemoTeamServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringMicroservicesDemoUserServiceApplication.class, args);
+		SpringApplication.run(SpringMicroservicesDemoTeamServiceApplication.class, args);
 	}
 
 }
---
+++ spring-microservices-demo-team-service/src/test/java/pl/adamberezka/springmicroservicesdemoteamservice/SpringMicroservicesDemoTeamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adamberezka.springmicroservicesdemoteamservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroservicesDemoTeamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
